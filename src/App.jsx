7210f8969aafa2eea07a2082276f523e06bc869e
omar <omar>
2024-02-01 14:10:34 +0100

    results design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import UserInputs from "./components/UserInputs";
+import Results from "./components/Results";
 
 function App() {
   return (
     <>
       <Header />
       <UserInputs />
+      <Results />
     </>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,13 @@
+export default function Results() {
+  return (
+    <table id="result">
+      <thead>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Intereset (Year)</th>
+        <th>Total Intereset</th>
+        <th>Invested Capital</th>
+      </thead>
+    </table>
+  );
+}
